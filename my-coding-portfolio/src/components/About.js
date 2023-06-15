7719e1aedbe47b85977c6924f5f68c6877b777e2
malachigamblin <malachi.gamblin@gmail.com>
2023-06-14 20:37:28 -0400

    setup about.js

---
+++ my-coding-portfolio/src/components/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <h2>About Me</h2>
+        <p>
+          Welcome to my coding portfolio! My name is Malachi, and I am a Full
+          Stack Web Developer currently enrolled in The Ohio State University
+          Coding Boot Camp. With a lifelong passion for technology and its
+          potential to solve real-world problems, I am thrilled to pursue my
+          dream of becoming a developer. What I find most intriguing about
+          coding is its duality, allowing me to be both creative and logical at
+          the same time. I thrive on taking complex problems and breaking them
+          down into manageable pieces, only to then put them back together in a
+          seamless manner. Thank you for visiting my portfolio. I invite you to
+          explore my work, and please don't hesitate to get in touch if you have
+          any questions or would like to connect.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
